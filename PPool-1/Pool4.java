7bd4521cf0af3e9bd6cf0522e11cf529d02da030
miguelet <miguelettortosa@gmail.com>
2016-02-24 20:39:45 +0100

    enteoria asta pisina 4

--- PPool-1/Pool4.java
+++ PPool-1/Pool4.java
@@ -1,9 +1,78 @@
-// CSD feb 2013 Juansa Sendra
 
- 
+//CSD feb 2013 Juansa Sendra
+public class Pool4 extends Pool0 {    // kids cannot enter if there are instructors waiting to exit
 
-public class Pool4 extends Pool0 { //kids cannot enter if there are instructors waiting to exit
-	
-  // To be completed
+    // To be completed
+    private int     contador_xiquets      = 0;
+    private int     contador_instructor   = 0;
+    private int     capacidad             = 0;
+    private boolean instructores_saliendo = false;
 
+    public synchronized long instructorRests(int id) {
+        while (((contador_instructor == 1) && (contador_xiquets > 0))
+                || (contador_xiquets + 1 >= contador_instructor * log.maxKI())) {    // ||  ((maxKI / contador_instructor) <  contador_instructor   )
+            instructores_saliendo = true;
+            log.leaveWait(id);
+            try {
+                wait();
+            } catch (Exception e) {}
+        }
+
+        if (contador_instructor > 0) {
+            contador_instructor--;
+            capacidad--;
+            instructores_saliendo = false;
+        }
+        notifyAll();
+        return log.rests(id);
+    }
+
+    public synchronized long instructorSwims(int id) {
+        while (capacidad > 4) {
+            try {
+                log.enterWait(id);
+                wait();
+            } catch (Exception e) {}
+        }
+
+        contador_instructor++;
+        capacidad++;
+        notifyAll();
+        return log.swims(id);
+    }
+
+    public synchronized long kidRests(int id) {
+        if (contador_xiquets > 0) {
+            contador_xiquets--;
+            capacidad--;
+        }
+        notifyAll();
+        return log.rests(id);
+    }
+
+    public synchronized long kidSwims(int id) {
+        while (((contador_instructor == 0) || (instructores_saliendo == true))
+                || (contador_xiquets + 1 >= contador_instructor * log.maxKI())
+                || (capacidad > log.capacity())) {
+            log.enterWait(id);
+            try {
+
+                wait();
+            } catch (Exception e) {
+
+            }
+        }
+
+        contador_xiquets++;
+        capacidad++;
+       // notifyAll();
+        return log.swims(id);
+    }
+
+    public synchronized void make(Log log1) {
+        log = log1;
+    }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
